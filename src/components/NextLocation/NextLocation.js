a118420ff036ad32337eb9c9aab65619100bb403
Matt Lemche <mattlemche@gmail.com>
2021-05-02 15:27:16 -0400

    add styling for sections

--- src/components/NextLocation/NextLocation.js
+++ src/components/NextLocation/NextLocation.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import bukitLambir from '../../assets/ej-yao-IAZ__Cm1psk-unsplash-2.jpg';
+import { ReactComponent as TattooTwo } from '../../assets/tattoo-2022.svg'; 
 
 function NextLocation() {
     return (
@@ -6,6 +8,18 @@ function NextLocation() {
             <h3 className="heading-3">
             Where to Next?
             </h3>
+            <div className="section__content">
+                <div className="stacked stacked--right">
+                        <img src={bukitLambir} alt="View of mountains in Malaysian Borneo" className="stacked__image stacked__image--landscape"/>
+                        <TattooTwo className="stacked__svg stacked__svg--landscape"/>
+
+                </div>
+                <p className="section__paragraph section__paragraph--right">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                    
+            </div>
+            
         </section>
     );
 }
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -7,10 +7,11 @@ function Video() {
           <h3 className="heading-3">
               Watch Episode One
           </h3>
-          <div className="i-frame"><iframe title='decades-ep-1' src='https://vimeo.com/showcase/8423191/embed' allowFullScreen frameBorder='0'></iframe></div>
           <p className="paragraph">
-            This film would not have been possibble without the help of <Link to="/supporters" className="inline-link">these generous supporters</Link> 
+            The installment you are about to watch would not have been possible without the help of <Link to="/supporters" className="inline-link">these generous supporters</Link> 
           </p>
+          <div className="video"><iframe title='decades-ep-1' className="video__i-frame" src='https://vimeo.com/showcase/8423191/embed' allowFullScreen frameBorder='0'></iframe></div>
+          
 
         </section>
     );
