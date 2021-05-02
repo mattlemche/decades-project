e6c4565e2bf10445f36b64ddd546a662b1c08bc6
Matt Lemche <mattlemche@gmail.com>
2021-05-02 18:20:54 -0400

    correct errors in target blank linking

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,11 +9,11 @@ function Footer() {
                 <h4 className="heading-4">
                 Contact
                 </h4>
-                <a href="https://twitter.com/DecadesFilm" className="footer__contact" target="_blank">
+                <a href="https://twitter.com/DecadesFilm" className="footer__contact" target="_blank" rel="noreferrer">
                  <Twitter className="footer__social"/> @decadesproject
                 </a>
 
-                <a href="mailto:matt@decadesproject.com" className="footer__contact" target="_blank">
+                <a href="mailto:matt@decadesproject.com" className="footer__contact" target="_blank" rel="noreferrer">
                  <Email className="footer__social"/> info@decadesproject.com
                 </a>
                 
