034f29e7292053afe79b4e30154c7cac8ffe7479
Matt Lemche <mattlemche@gmail.com>
2021-05-03 12:26:28 -0400

    Add social links for Marg

--- src/data.js
+++ src/data.js
@@ -65,8 +65,8 @@ export const profiles = [
         alt: 'Photo of Marguerite at Laufas Church.',
         bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         socials: {
-        twitter: '',
-        instagram: '',
+        twitter: 'https://twitter.com/MargueriteMorea',
+        instagram: 'https://www.instagram.com/margueritemoreau',
         website: '',
         other: '',
         }
