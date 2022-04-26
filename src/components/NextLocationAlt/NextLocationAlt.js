9cf8157c87ea4af0c195f42b0d9750684256367a
Matt Lemche <mattlemche@gmail.com>
2022-04-25 22:08:32 -0400

    set up for video credits and correct i-frame padding

--- src/components/NextLocationAlt/NextLocationAlt.js
+++ src/components/NextLocationAlt/NextLocationAlt.js
@@ -8,7 +8,8 @@ function NextLocation() {
         <section className="section">
             
             <div>
-            <div className="video"><iframe title="Decades 2022" className="video__i-frame" src="https://player.vimeo.com/video/702549278?h=3e2bdc6c21" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture"></iframe></div>                 
+            <div className="video"><iframe title="Decades 2022" className="video__i-frame" src="https://player.vimeo.com/video/702549278?h=3e2bdc6c21" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture"></iframe></div>     
+            <p className="fine-print fine-print--center">{/*This is where the credits will go for video and music.*/}</p>            
             </div>
             
         </section>
