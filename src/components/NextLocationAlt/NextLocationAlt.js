5314b5eec8e055e63940f03f8930410036620f56
Matt Lemche <mattlemche@gmail.com>
2022-04-26 21:13:11 -0300

    Add credits for additional footage and music to 2022 promo video embed.

--- src/components/NextLocationAlt/NextLocationAlt.js
+++ src/components/NextLocationAlt/NextLocationAlt.js
@@ -9,7 +9,8 @@ function NextLocation() {
             
             <div>
             <div className="video"><iframe title="Decades 2022" className="video__i-frame" src="https://player.vimeo.com/video/702549278?h=3e2bdc6c21" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture"></iframe></div>     
-            <p className="fine-print fine-print--center">{/*This is where the credits will go for video and music.*/}</p>            
+            <p className="fine-print fine-print--center">{/*This is where the credits will go for video and music.*/}Additional footage from <a href="https://www.pexels.com/">Pexels</a> from contributers <a href="https://www.pexels.com/video/the-sunset-view-captured-by-a-drone-camera-4563312/">Motion Media</a>, <a href="https://www.pexels.com/video/close-up-of-a-static-television-8546323/">Valiantsin Konan</a>, <a href="https://www.pexels.com/video/aerial-footage-of-a-town-in-iceland-9881117/">Gylfi Gylfason</a>, and <a href="https://www.pexels.com/video/drone-footage-of-the-trees-canopy-of-a-thick-forest-2491285/">Kelly L</a>.</p>
+            <p className="fine-print fine-print--center">Music from <a href="https://artlist.io/">Artlist from contributers kutiman and tristan-barton</a>.</p>        
             </div>
             
         </section>
