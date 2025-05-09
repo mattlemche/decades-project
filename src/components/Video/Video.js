5b14789c686909d444c80864f8f5368143e4d8f7
Matt Lemche <mattlemche@gmail.com>
2025-05-09 14:46:28 -0400

    Update vime link to display properly

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -10,7 +10,16 @@ function Video() {
           <p className="paragraph">
             This installment, Episode 1: The Rowan Tree, would not have been possible without the help of <Link to="/supporters" className="inline-link">these generous supporters</Link> 
           </p>
-          <div className="video"><iframe title='decades-ep-1' className="video__i-frame" src='https://vimeo.com/showcase/8423191/embed' allowFullScreen frameBorder='0'></iframe></div>
+          <div className="video"><iframe src="https://player.vimeo.com/video/179073673" 
+            width="640" 
+            height="360" 
+            frameborder="0"
+            className="video__i-frame"
+            title="Decades Epsidoe 1" 
+            allow="autoplay; fullscreen; picture-in-picture" 
+            allowfullscreen>
+          </iframe></div>
+          
           
 
         </section>
